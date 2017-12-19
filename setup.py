738b8ec44bf919dc94148e7928a3fcc81bde2b6a
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2017-12-19 17:43:08 -0500

    changed opt parse to arg parse for python3

--- setup.py
+++ setup.py
@@ -73,12 +73,13 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
-        #'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
@@ -99,7 +100,6 @@ setup(
     install_requires=[
         'boto3>=1.3.1'
         'pycrypto>=2.6.1'
-        'argparse>=1.2.1'
     ],
 
     # List additional groups of dependencies here (e.g. development
